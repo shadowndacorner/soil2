ef26b2a0f66ee17c3994dc1497ad5c30e7d1520b
Ian Diaz <shadowndacorner@gmail.com>
2017-05-29 23:25:15 -0500

    Cleaned up repository, formatted more to my liking

--- src/SOIL2.c
+++ src/SOIL2.c
@@ -104,16 +104,16 @@
 #define GL_RG                             0x8227
 #endif
 
-#include "SOIL2.h"
+#include <SOIL2.h>
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include <stb_image.h>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-#include "image_helper.h"
-#include "image_DXT.h"
-#include "pvr_helper.h"
-#include "pkm_helper.h"
-#include "jo_jpeg.h"
+#include <stb_image_write.h>
+#include <image_helper.h>
+#include <image_DXT.h>
+#include <pvr_helper.h>
+#include <pkm_helper.h>
+#include <jo_jpeg.h>
 
 #include <stdlib.h>
 #include <string.h>
--- src/etc1_utils.c
+++ src/etc1_utils.c
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "etc1_utils.h"
-
+#include <etc1_utils.h>
 #include <string.h>
 
 /* From http://www.khronos.org/registry/gles/extensions/OES/OES_compressed_ETC1_RGB8_texture.txt
